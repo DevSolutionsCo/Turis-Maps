f0f12549b6d7c35c84b56043aa7fa5e193434cd5
Seebastian Jara <sebasjara1503@outlook.com>
2023-05-23 11:10:16 -0600

    te odio javi terco

---
+++ sql/turismaps.sql
@@ -0,0 +1,129 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema turismaps
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema turismaps
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `turismaps` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `turismaps` ;
+
+-- -----------------------------------------------------
+-- Table `turismaps`.`lugar`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `turismaps`.`lugar` (
+  `id_lugar` INT NOT NULL,
+  `nombre_lugar` VARCHAR(40) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  PRIMARY KEY (`id_lugar`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `turismaps`.`turista`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `turismaps`.`turista` (
+  `id_turista` INT NOT NULL AUTO_INCREMENT,
+  `correo` VARCHAR(30) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `nombre` VARCHAR(40) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `apellido_pat` VARCHAR(40) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `apellido_mat` VARCHAR(40) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `fecha_nac` DATE NULL DEFAULT NULL,
+  `lugar_proc` VARCHAR(30) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `genero` VARCHAR(10) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `contrasena` VARCHAR(20) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `nombre_user` VARCHAR(45) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  PRIMARY KEY (`id_turista`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `turismaps`.`comentarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `turismaps`.`comentarios` (
+  `id_comentario` INT NOT NULL auto_increment,
+  `comentario` VARCHAR(100) CHARACTER SET 'utf8mb3' NULL DEFAULT NULL,
+  `calificacion` INT NULL DEFAULT NULL,
+  `fecha` DATE NULL DEFAULT NULL,
+  `id_turista` INT NULL DEFAULT NULL,
+  `id_lugar` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`id_comentario`),
+  INDEX `id_lugar` (`id_lugar` ASC) VISIBLE,
+  INDEX `id_turista` (`id_turista` ASC) VISIBLE,
+  CONSTRAINT `comentarios_ibfk_1`
+    FOREIGN KEY (`id_lugar`)
+    REFERENCES `turismaps`.`lugar` (`id_lugar`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `comentarios_ibfk_2`
+    FOREIGN KEY (`id_turista`)
+    REFERENCES `turismaps`.`turista` (`id_turista`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `turismaps`.`lugar_comentarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `turismaps`.`lugar_comentarios` (
+  `id_comentario` INT NULL DEFAULT NULL,
+  `id_lugar` INT NULL DEFAULT NULL,
+  INDEX `id_comentario` (`id_comentario` ASC) VISIBLE,
+  INDEX `id_lugar` (`id_lugar` ASC) VISIBLE,
+  CONSTRAINT `lugar_comentarios_ibfk_1`
+    FOREIGN KEY (`id_comentario`)
+    REFERENCES `turismaps`.`comentarios` (`id_comentario`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `lugar_comentarios_ibfk_2`
+    FOREIGN KEY (`id_lugar`)
+    REFERENCES `turismaps`.`lugar` (`id_lugar`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `turismaps`.`turista_comentarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `turismaps`.`turista_comentarios` (
+  `id_comentario` INT NULL DEFAULT NULL,
+  `id_turista` INT NULL DEFAULT NULL,
+  INDEX `id_comentario` (`id_comentario` ASC) VISIBLE,
+  INDEX `id_turista` (`id_turista` ASC) VISIBLE,
+  CONSTRAINT `turista_comentarios_ibfk_1`
+    FOREIGN KEY (`id_comentario`)
+    REFERENCES `turismaps`.`comentarios` (`id_comentario`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `turista_comentarios_ibfk_2`
+    FOREIGN KEY (`id_turista`)
+    REFERENCES `turismaps`.`turista` (`id_turista`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
---
+++ src/java/org/turis/helper/TuristaHelper.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.turis.helper;
+
+import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.turis.dao.Turista;
+import org.turis.service.TuristaService;
+
+/**
+ *
+ * @author ruizl
+ */
+public class TuristaHelper extends Helpers<Turista> implements Serializable {
+
+    private TuristaService turistaService;
+
+    public TuristaHelper() {
+    }
+
+    public boolean isValidaCamposOk() {
+        return  isNotNullAndNotEmpity(t.getCorreo())
+                && isNotNullAndNotEmpity(t.getNombre()) && isNotNullAndNotEmpity(t.getApellido_pat())
+                && isNotNullAndNotEmpity(t.getApellido_mat()) && isNotNull(t.getFecha_nac())
+                && isNotNullAndNotEmpity(t.getLugar_proc()) && isNotNullAndNotEmpity(t.getGenero())
+                && isNotNullAndNotEmpity(t.getContraseña()) && isNotNullAndNotEmpity(t.getNombre_user());
+    }
+
+    @Override
+    public boolean addT() {
+        turistaService = new TuristaService();
+        t = new Turista();
+        
+        t.setNombre_user(getParameter("nombre_user"));
+        t.setCorreo(getParameter("correo"));
+        t.setNombre(getParameter("nombre"));
+        t.setApellido_pat(getParameter("apellido_pat"));
+        t.setApellido_mat(getParameter("apellido_mat"));
+        t.setFecha_nac(string2Date(getParameter("fecha_nac")));
+        t.setLugar_proc(getParameter("lugar_proc"));
+        t.setGenero(getParameter("genero"));
+        t.setContraseña(getParameter("contraseña"));
+        if (isValidaCamposOk()) {
+            return turistaService.addTurista(t);
+        }
+
+        return false;
+    }
+
+    @Override
+    public List<Turista> getListT() {
+        turistaService = new TuristaService();
+        return turistaService.getTuristaList();
+    }
+
+    @Override
+    public boolean updateT() {
+        turistaService = new TuristaService();
+        t = new Turista();
+       
+        t.setCorreo(getParameter("correo"));
+        t.setNombre(getParameter("nombre"));
+        t.setApellido_pat(getParameter("apellido_pat"));
+        t.setApellido_mat(getParameter("apellido_mat"));
+        t.setFecha_nac(string2Date("fecha_nac"));
+        t.setLugar_proc(getParameter("lugar_proc"));
+        t.setGenero(getParameter("genero"));
+        t.setContraseña(getParameter("contraseña"));
+        t.setNombre_user(getParameter("nombre_user"));
+        if (isValidaCamposOk()) {
+            return turistaService.updateTurista(t);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteT() {
+        turistaService = new TuristaService();
+        t = new Turista();
+        t.setId_turista(Integer.parseInt(getParameter("rol")));
+        if (t.getId_turista() != null && t.getId_turista() > 0) {
+            return turistaService.deleteTurista(t);
+        }
+        return false;
+    }
+
+    @Override
+    public Turista getTByKey() {
+        String correo = null;
+        correo = getParameter("correo");
+        if (correo == null || correo.length() <= 0) {
+            return null;
+        }
+        turistaService = new TuristaService();
+        return  turistaService.getTuristaByCorreo(correo);
+    }
+
+}
